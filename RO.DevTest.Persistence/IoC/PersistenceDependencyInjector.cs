dc13c736c9d0ff9dbef9481c83a302434502f64f
unknown <athamsmenezes@gmail.com>
2025-04-20 18:19:25 -0300

    first commit

---
+++ RO.DevTest.Persistence/IoC/PersistenceDependencyInjector.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Infrastructure;
+
+namespace RO.DevTest.Persistence.IoC;
+
+public static class PersistenceDependencyInjector {
+    /// <summary>
+    /// Inject the dependencies of the Persistence layer into an
+    /// <see cref="IServiceCollection"/>
+    /// </summary>
+    /// <param name="services">
+    /// The <see cref="IServiceCollection"/> to inject the dependencies into
+    /// </param>
+    /// <returns>
+    /// The <see cref="IServiceCollection"/> with dependencies injected
+    /// </returns>
+    public static IServiceCollection InjectPersistenceDependencies(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+        services.AddDbContext<DefaultContext>(options =>
+            options.UseNpgsql(connectionString));
+
+        return services;
+    }
+}
