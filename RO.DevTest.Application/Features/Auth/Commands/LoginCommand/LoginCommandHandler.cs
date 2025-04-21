103f652c09d8ed51d8b123b7e778c6a970f18b3a
unknown <athamsmenezes@gmail.com>
2025-04-21 18:30:14 -0300

    feat(auth): criar login handle em LoginCommandHandle

--- RO.DevTest.Application/Features/Auth/Commands/LoginCommand/LoginCommandHandler.cs
+++ RO.DevTest.Application/Features/Auth/Commands/LoginCommand/LoginCommandHandler.cs
@@ -1,11 +1,43 @@
-﻿using MediatR;
+﻿using System.Data;
+using MediatR;
+using RO.DevTest.Application.Contracts.Infrastructure; 
+using RO.DevTest.Domain.Exception;
 
 namespace RO.DevTest.Application.Features.Auth.Commands.LoginCommand;
 
 public class LoginCommandHandler : IRequestHandler<LoginCommand, LoginResponse> {
-    public Task<LoginResponse> Handle(LoginCommand request, CancellationToken cancellationToken) {
-        ///[TODO] - CREATE LOGIN HANDLER HERE    
+    private readonly IIdentityAbstractor _identityAbstractor;
 
-        throw new NotImplementedException();
+    public LoginCommandHandler(IIdentityAbstractor identityAbstractor){
+        _identityAbstractor = identityAbstractor;
+    }
+    public async Task<LoginResponse> Handle(LoginCommand request, CancellationToken cancellationToken) {
+        // verfic if user exist wiht email
+        var user = await _identityAbstractor.FindUserByEmailAsync(request.Email);
+        if(user == null){
+            throw new BadRequestException("Usuario não existe");
+        }
+       
+        // confirm login wiht password
+        var result = await _identityAbstractor.PasswordSignInAsync(user, request.Password);
+        if(!result.Succeeded){
+            throw new BadRequestException("Usuario ou senha inválidas");
+        }
+
+        var role = await _identityAbstractor.GetUserRolesAsync(user);
+        
+        // generet token
+        string accessToken = "teste-access-token";
+        string refreshToken = "teste-refresh-token";
+
+        var now = DateTime.UtcNow;
+        var reponse = new LoginResponse{
+            AccessToken = accessToken,
+            RefreshToken = refreshToken,
+            IssuedAt = now,
+            ExpirationDate = now.AddMinutes(60),
+            Roles = role
+        };
+        return reponse;
     }
 }
