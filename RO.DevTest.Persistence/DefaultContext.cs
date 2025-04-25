cbe94f9e669f432e88d765512fd08858adb88a16
unknown <athamsmenezes@gmail.com>
2025-04-25 11:54:28 -0300

    feat(product) adiciona produto

--- RO.DevTest.Persistence/DefaultContext.cs
+++ RO.DevTest.Persistence/DefaultContext.cs
@@ -6,14 +6,13 @@ namespace RO.DevTest.Persistence;
 
 public class DefaultContext : IdentityDbContext<User> {
 
-    public DefaultContext() { }
-
     public DefaultContext(DbContextOptions<DefaultContext> options) : base(options) { }
-
+    public DbSet<Product> Products { get; set; } = null!;
     protected override void OnModelCreating(ModelBuilder builder) {
         builder.HasPostgresExtension("uuid-ossp");
         builder.ApplyConfigurationsFromAssembly(typeof(DefaultContext).Assembly);
 
         base.OnModelCreating(builder);
+        
     }
 }
