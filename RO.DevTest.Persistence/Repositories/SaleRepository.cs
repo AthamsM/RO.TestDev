50ec533716d6cbc5cc05299d7b5f6fc034d4a90f
unknown <athamsmenezes@gmail.com>
2025-04-27 22:00:29 -0300

    feat(sale): implementa o metodo GetAllSale

--- RO.DevTest.Persistence/Repositories/SaleRepository.cs
+++ RO.DevTest.Persistence/Repositories/SaleRepository.cs
@@ -1,5 +1,24 @@
 using System.ComponentModel;
+using Microsoft.EntityFrameworkCore;
 using RO.DevTest.Persistence;
 using RO.DevTest.Persistence.Repositories;
 
-public class SaleRepository(DefaultContext context) : BaseRepository<Sale>(context), ISaleRepository{}
+public class SaleRepository : BaseRepository<Sale>, ISaleRepository
+{
+    private readonly DefaultContext _context;
+    public SaleRepository(DefaultContext context) : base(context) {
+        _context = context;
+     }
+
+    public async Task<List<Sale>> GetSalesByPeriod(DateTime startDate, DateTime endDate, CancellationToken cancellationToken = default)
+    {
+        return await _context.Set<Sale>()
+            .Include(s => s.Product) // para poder pegar o nome do produto no Handler
+            .Where(s => s.SaleDate >= startDate && s.SaleDate <= endDate)
+            .ToListAsync(cancellationToken);
+    }
+
+    public async Task<List<Sale>> GetAllSale(){
+        return await _context.Sales.ToListAsync();
+    }
+}
