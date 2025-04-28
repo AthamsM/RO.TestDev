bf0730d5a1ec70b3cb2618a3ef19511a18031001
unknown <athamsmenezes@gmail.com>
2025-04-27 21:59:39 -0300

    feat(sale): adciona na interface o GetAllSale

--- RO.DevTest.Application/Contracts/Persistance/Repositories/ISaleRepository.cs
+++ RO.DevTest.Application/Contracts/Persistance/Repositories/ISaleRepository.cs
@@ -1,3 +1,6 @@
 using RO.DevTest.Application.Contracts.Persistance.Repositories;
 
-public interface ISaleRepository : IBaseRepository<Sale>;
+public interface ISaleRepository : IBaseRepository<Sale>{
+    Task<List<Sale>> GetSalesByPeriod(DateTime startDate, DateTime endDate, CancellationToken cancellationToken = default);
+    Task<List<Sale>> GetAllSale();
+}
